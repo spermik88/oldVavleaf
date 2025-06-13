2bf7b24f2e2e53f88bfc85279ed69f3538c60a6b
spermik88 <serzhez79@gmail.com>
2025-06-13 11:59:35 +0400

    Добавлен moduleNameMapper в конфиг Jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   preset: 'jest-expo',
   moduleFileExtensions: ['ts', 'tsx', 'js'],
+  moduleNameMapper: {
+    '^react-native$': 'react-native'
+  },
   setupFilesAfterEnv: ['@testing-library/react-native']
 };
