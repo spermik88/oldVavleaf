5f38127b527111a655e4c20e930b44f4154fd587
spermik88 <serzhez79@gmail.com>
2025-06-13 12:21:50 +0400

    test: wrap LeafContour renderer calls in act

--- __tests__/LeafContour.test.tsx
+++ __tests__/LeafContour.test.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import renderer, { act } from 'react-test-renderer';
 import LeafContour from '../components/LeafContour';
 
 describe('LeafContour', () => {
   it('renders without crashing when points are provided', () => {
     expect(() => {
-      renderer.create(
-        <LeafContour points={[{ x: 0, y: 0 }, { x: 1, y: 1 }]} />
-      );
+      act(() => {
+        renderer.create(
+          <LeafContour points={[{ x: 0, y: 0 }, { x: 1, y: 1 }]} />
+        );
+      });
     }).not.toThrow();
   });
 
   it('returns null when no points provided', () => {
-    const tree = renderer.create(<LeafContour points={[]} />).toJSON();
-    expect(tree).toBeNull();
+    let tree;
+    act(() => {
+      tree = renderer.create(<LeafContour points={[]} />);
+    });
+    expect(tree.toJSON()).toBeNull();
   });
 });
